f7c8ce114a77a0c18f74a151361feacd67bb6318
KittyZhao <zhaojunjie1113@126.com>
2015-11-22 16:40:10 +0800

    初始化工程

---
+++ app/src/main/java/com/charsunny/poem/MainActivity.java
@@ -0,0 +1,140 @@
+package com.charsunny.poem;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    protected Toolbar toolbar;
+    protected FloatingActionButton fab;
+    public int currentId = R.id.nav_camera;
+    private RecomandFragment recomandFragment;
+    private ColumnFragment columnFragment;
+    private DiscorveryFragment discorveryFragment;
+    private FavFragment favFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        setDefaultFragment();
+    }
+
+    public void setDefaultFragment() {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        RecomandFragment rf = new RecomandFragment();
+        ft.replace(R.id.id_content, rf);
+        ft.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        if (id == R.id.nav_camera) {
+            toolbar.setTitle("诗词");
+            if (recomandFragment == null) {
+                recomandFragment = new RecomandFragment();
+            }
+            transaction.replace(R.id.id_content, recomandFragment);
+        } else if (id == R.id.nav_gallery) {
+            toolbar.setTitle("发现");
+            toolbar.inflateMenu(R.menu.search);
+            if (discorveryFragment == null) {
+                discorveryFragment = new DiscorveryFragment();
+            }
+            transaction.replace(R.id.id_content, discorveryFragment);
+        } else if (id == R.id.nav_slideshow) {
+            toolbar.setTitle("专栏");
+            if (columnFragment == null) {
+                columnFragment = new ColumnFragment();
+            }
+            transaction.replace(R.id.id_content, columnFragment);
+        } else if (id == R.id.nav_manage) {
+            toolbar.setTitle("收藏");
+            if (favFragment == null) {
+                favFragment = new FavFragment();
+            }
+            transaction.replace(R.id.id_content, favFragment);
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+        transaction.commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/charsunny/poem/RecomandFragment.java
@@ -0,0 +1,19 @@
+package com.charsunny.poem;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by junjiezhao on 15/11/22.
+ */
+public class RecomandFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_main, container, false);
+    }
+}
